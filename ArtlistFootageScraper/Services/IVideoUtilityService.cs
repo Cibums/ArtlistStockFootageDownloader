fddf93787359a97ed92be655c284d910a13d9e69
Cibums <lucas.fransson@hotmail.com>
2023-09-30 17:18:38 +0200

    Refactored UtilityService

--- ArtlistFootageScraper/Services/IVideoUtilityService.cs
+++ ArtlistFootageScraper/Services/IVideoUtilityService.cs
@@ -4,11 +4,34 @@ namespace ArtlistFootageScraper.Services
 {
     public interface IVideoUtilityService
     {
-        public OptimizedFootageData GetOptimizedFootageData(string path, float lengthSeconds);
-        public string MergeVideoAndAudio(string videoPath, string audioPath);
-        public List<Point> ExtractPointsBetweenIndexes(List<Point> points, int startIndex, int endIndex);
-        public Point CalculateAveragePoint(List<Point> points);
-        public int IndexOfMaxSumSubsequence(List<int> numbers, int length);
-        public TimeSpan GetWavDuration(string filePath);
+        /// <summary>
+        /// Gets optimized footage data.
+        /// </summary>
+        OptimizedFootageData GetOptimizedFootageData(string path, float lengthSeconds);
+
+        /// <summary>
+        /// Merges video and audio.
+        /// </summary>
+        string MergeVideoAndAudio(string videoPath, string audioPath);
+
+        /// <summary>
+        /// Extracts points between given indexes.
+        /// </summary>
+        List<Point> ExtractPointsBetweenIndexes(List<Point> points, int startIndex, int endIndex);
+
+        /// <summary>
+        /// Calculates the average point.
+        /// </summary>
+        Point CalculateAveragePoint(List<Point> points);
+
+        /// <summary>
+        /// Finds the index of the maximum sum subsequence.
+        /// </summary>
+        int IndexOfMaxSumSubsequence(List<int> numbers, int length);
+
+        /// <summary>
+        /// Gets the duration of a WAV file.
+        /// </summary>
+        TimeSpan GetWavDuration(string filePath);
     }
 }
--- ArtlistFootageScraper/Services/VideoUtilityService.cs
+++ ArtlistFootageScraper/Services/VideoUtilityService.cs
@@ -7,6 +7,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
 
 namespace ArtlistFootageScraper.Services
 {
@@ -14,128 +15,129 @@ namespace ArtlistFootageScraper.Services
     {
         private static readonly string outputPath = AppConfiguration.stockFootageOutputPath;
         private readonly IVideoAnalysisService _analysisService;
+        private readonly ILogger<VideoUtilityService> _logger;
 
-        public VideoUtilityService(IVideoAnalysisService analysisService)
+        public VideoUtilityService(IVideoAnalysisService analysisService, ILogger<VideoUtilityService> logger)
         {
-            _analysisService = analysisService;
+            _analysisService = analysisService ?? throw new ArgumentNullException(nameof(analysisService));
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
         }
 
+        /// <summary>
+        /// Gets optimized footage data.
+        /// </summary>
         public OptimizedFootageData GetOptimizedFootageData(string path, float lengthSeconds)
         {
-            VideoCapture capture = new VideoCapture(path);
+            var capture = new VideoCapture(path);
             var framerate = capture.Get(CapProp.Fps);
-            VideoAnalysisResult videoAnalysisResult = _analysisService.AnalyzeVideo(path, capture);
-            int startFrame = IndexOfMaxSumSubsequence(videoAnalysisResult.ActionPerFrame, (int)Math.Round((float)framerate * lengthSeconds));
-            int endFrame = startFrame + (int)Math.Round((float)framerate * lengthSeconds);
-            List<Point> points = ExtractPointsBetweenIndexes(videoAnalysisResult.MaxActionCoordinatesPerFrame, startFrame, endFrame);
-            Point focusPoint = points != null ? CalculateAveragePoint(points) : new Point(80, 60);
+            var videoAnalysisResult = _analysisService.AnalyzeVideo(path, capture);
+            var startFrame = IndexOfMaxSumSubsequence(videoAnalysisResult.ActionPerFrame, (int)Math.Round(framerate * lengthSeconds));
+            var endFrame = startFrame + (int)Math.Round(framerate * lengthSeconds);
+            var points = ExtractPointsBetweenIndexes(videoAnalysisResult.MaxActionCoordinatesPerFrame, startFrame, endFrame);
+            var focusPoint = points != null && points.Any() ? CalculateAveragePoint(points) : new Point(80, 60);
+
             return new OptimizedFootageData(framerate, lengthSeconds, startFrame, focusPoint);
         }
 
+        /// <summary>
+        /// Merges video and audio.
+        /// </summary>
         public string MergeVideoAndAudio(string videoPath, string audioPath)
         {
-            int rndNumber = new Random().Next(0, 10000000);
-            string outputP = outputPath + $"\\{rndNumber}merged_output.mp4";
-            string ffmpegExecutablePath = AppConfiguration.FFMPEG_EXE_PATH + @"\ffmpeg.exe";
+            var rndNumber = new Random().Next(0, 10000000);
+            var outputFilePath = Path.Combine(outputPath, $"{rndNumber}merged_output.mp4");
+            var ffmpegExecutablePath = Path.Combine(AppConfiguration.FFMPEG_EXE_PATH, "ffmpeg.exe");
 
             try
             {
-                ProcessStartInfo psi = new ProcessStartInfo();
-                psi.FileName = ffmpegExecutablePath;
-                psi.Arguments = $"-i \"{videoPath}\" -itsoffset 0.5 -i \"{audioPath}\" -c:v copy -c:a aac \"{outputP}\"";
-                psi.RedirectStandardOutput = true;
-                psi.RedirectStandardError = true;
-                psi.UseShellExecute = false;
-                psi.CreateNoWindow = true;
+                var psi = new ProcessStartInfo
+                {
+                    FileName = ffmpegExecutablePath,
+                    Arguments = $"-i \"{videoPath}\" -itsoffset 0.5 -i \"{audioPath}\" -c:v copy -c:a aac \"{outputFilePath}\"",
+                    RedirectStandardOutput = true,
+                    RedirectStandardError = true,
+                    UseShellExecute = false,
+                    CreateNoWindow = true
+                };
 
-                Process process = new Process { StartInfo = psi };
-
-                List<string> outputLines = new List<string>();
+                using var process = new Process { StartInfo = psi };
 
                 process.OutputDataReceived += (sender, e) =>
                 {
-                    if (!String.IsNullOrEmpty(e.Data))
-                        outputLines.Add(e.Data);
+                    if (!string.IsNullOrEmpty(e.Data))
+                        _logger.LogInformation(e.Data);
                 };
 
                 process.ErrorDataReceived += (sender, e) =>
                 {
-                    if (!String.IsNullOrEmpty(e.Data))
-                        Console.WriteLine("Error: " + e.Data);  // or log it, or store it for later analysis
+                    if (!string.IsNullOrEmpty(e.Data))
+                        _logger.LogError($"Error: {e.Data}");
                 };
 
                 process.Start();
-
-                process.BeginOutputReadLine();  // Start reading output asynchronously
+                process.BeginOutputReadLine();
                 process.BeginErrorReadLine();
-
                 process.WaitForExit();
             }
             catch (Exception ex)
             {
-                Console.WriteLine("Exception: " + ex.Message);
+                _logger.LogError($"Exception during merging video and audio: {ex.Message}");
                 throw;
             }
 
-            Console.WriteLine("FFmpeg process completed.");
-
-            return outputP;
+            _logger.LogInformation("FFmpeg process completed.");
+            return outputFilePath;
         }
 
+        /// <summary>
+        /// Extracts points between given indexes.
+        /// </summary>
         public List<Point> ExtractPointsBetweenIndexes(List<Point> points, int startIndex, int endIndex)
         {
             if (points == null)
-            {
                 throw new ArgumentNullException(nameof(points));
-            }
-
 
             if (startIndex < 0 || startIndex >= points.Count || endIndex < 0 || startIndex > endIndex)
-            {
                 throw new ArgumentOutOfRangeException("Invalid start or end index.");
-            }
 
-            //int endFrame = Math.Min(endIndex, points.Count);
             try
             {
-                List<Point> list = points.GetRange(startIndex, endIndex - startIndex);
-                return list;
+                return points.GetRange(startIndex, endIndex - startIndex);
             }
-            catch
+            catch (Exception ex)
             {
+                _logger.LogError($"Error extracting points: {ex.Message}");
                 return null;
             }
         }
 
+        /// <summary>
+        /// Calculates the average point.
+        /// </summary>
         public Point CalculateAveragePoint(List<Point> points)
         {
-            if (points == null || points.Count == 0)
+            if (points == null || !points.Any())
                 throw new ArgumentException("The list of points cannot be null or empty.");
 
-            int totalX = 0;
-            int totalY = 0;
-
-            foreach (var point in points)
-            {
-                totalX += point.X;
-                totalY += point.Y;
-            }
+            var totalX = points.Sum(point => point.X);
+            var totalY = points.Sum(point => point.Y);
 
             return new Point(totalX / points.Count, totalY / points.Count);
         }
 
+        /// <summary>
+        /// Finds the index of the maximum sum subsequence.
+        /// </summary>
         public int IndexOfMaxSumSubsequence(List<int> numbers, int length)
         {
             if (numbers == null || numbers.Count < length)
-            {
                 return 0;
-            }
 
-            int currentSum = numbers.Take(length).Sum();
-            int maxSum = currentSum;
-            int maxIndex = 0;
+            var currentSum = numbers.Take(length).Sum();
+            var maxSum = currentSum;
+            var maxIndex = 0;
 
-            for (int i = 1; i <= numbers.Count - length; i++)
+            for (var i = 1; i <= numbers.Count - length; i++)
             {
                 currentSum = currentSum - numbers[i - 1] + numbers[i + length - 1];
 
@@ -151,46 +153,41 @@ namespace ArtlistFootageScraper.Services
 
         public TimeSpan GetWavDuration(string filePath)
         {
-            using (FileStream fs = new FileStream(filePath, FileMode.Open, FileAccess.Read))
-            using (BinaryReader reader = new BinaryReader(fs))
-            {
-                // Read RIFF header
-                string chunkID = new string(reader.ReadChars(4));
-                if (chunkID != "RIFF")
-                    throw new Exception("Invalid file format");
+            using var fs = new FileStream(filePath, FileMode.Open, FileAccess.Read);
+            using var reader = new BinaryReader(fs);
 
-                reader.ReadUInt32(); // Chunk size
-                string format = new string(reader.ReadChars(4));
-                if (format != "WAVE")
-                    throw new Exception("Invalid file format");
+            // Read RIFF header
+            string chunkID = new string(reader.ReadChars(4));
+            if (chunkID != "RIFF")
+                throw new Exception("Invalid file format");
 
-                // Read fmt subchunk
-                string subchunk1ID = new string(reader.ReadChars(4));
-                if (subchunk1ID != "fmt ")
-                    throw new Exception("Invalid file format");
+            reader.ReadUInt32(); // Chunk size
+            string format = new string(reader.ReadChars(4));
+            if (format != "WAVE")
+                throw new Exception("Invalid file format");
 
-                int subchunk1Size = reader.ReadInt32();
-                short audioFormat = reader.ReadInt16();
-                short numChannels = reader.ReadInt16();
-                int sampleRate = reader.ReadInt32();
-                int byteRate = reader.ReadInt32();
-                short blockAlign = reader.ReadInt16();
-                short bitsPerSample = reader.ReadInt16();
+            // Read fmt subchunk
+            string subchunk1ID = new string(reader.ReadChars(4));
+            if (subchunk1ID != "fmt ")
+                throw new Exception("Invalid file format");
 
-                // Skip to data subchunk
-                reader.BaseStream.Seek(subchunk1Size - 16, SeekOrigin.Current);
-                string subchunk2ID = new string(reader.ReadChars(4));
-                if (subchunk2ID != "data")
-                    throw new Exception("Invalid file format");
+            int subchunk1Size = reader.ReadInt32();
+            int sampleRate = reader.ReadInt32();
+            short bitsPerSample = reader.ReadInt16();
 
-                int subchunk2Size = reader.ReadInt32();
+            // Skip to data subchunk
+            reader.BaseStream.Seek(subchunk1Size - 16, SeekOrigin.Current);
+            string subchunk2ID = new string(reader.ReadChars(4));
+            if (subchunk2ID != "data")
+                throw new Exception("Invalid file format");
 
-                // Calculate duration
-                double totalSamples = subchunk2Size / (bitsPerSample / 8.0);
-                double duration = totalSamples / sampleRate;
+            int subchunk2Size = reader.ReadInt32();
 
-                return TimeSpan.FromSeconds(duration);
-            }
+            // Calculate duration
+            double totalSamples = subchunk2Size / (bitsPerSample / 8.0);
+            double duration = totalSamples / sampleRate;
+
+            return TimeSpan.FromSeconds(duration);
         }
     }
 }
